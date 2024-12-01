86c15985a11c80fdd6e78ee4c695d0286fdbb786
Darrick Ross <dross56@jhu.edu>
2024-12-01 18:42:14 -0500

    day 1a

---
+++ 2024/12/01/a/py/01.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+
+INPUT_FILE_REL_PATH = "../input"
+
+
+# def read_input(input_file_path: str) -> tuple[list, list]:
+
+#     return (left, right)
+
+
+def get_difference(a: int, b: int) -> int:
+    return abs(a - b)
+
+
+def main():
+    left = []
+    right = []
+
+    file_path = os.path.join(os.path.dirname(__file__), INPUT_FILE_REL_PATH)
+
+    with open(file_path, "r") as file:
+        for line in file:
+            parts = line.split()
+            if len(parts) == 2:
+                left.append(int(parts[0]))
+                right.append(int(parts[1]))
+
+    left.sort()
+    right.sort()
+    # difference = []
+    total_difference = 0
+
+    for ll, rr in zip(left, right):
+        total_difference += get_difference(ll, rr)
+
+    print(total_difference)
+
+
+if __name__ == "__main__":
+    main()
